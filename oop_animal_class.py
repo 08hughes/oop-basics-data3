93f7811f7c93478eb197fc4919f4e8051562807d
Daniel Hughes <DHughes@spartaglobal.com>
2019-07-25 10:44:27 +0100

    func sleep and eat created

---
+++ oop_animal_class.py
@@ -0,0 +1,21 @@
+#
+# class Animal:
+#
+#     def __init__(self, ):
+#         pass
+#
+#     def sleep(self):
+#         return "Zzzzzzzz"
+#
+#     def eat(self, food):
+#         pass
+#
+#
+
+
+def sleep():
+    return "Zzzzzzzz"
+
+def eat(food):
+    return "animal is tired"
+
