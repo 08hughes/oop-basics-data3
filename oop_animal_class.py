94130cc52949425301bdf46b5ffd4e97e2becb05
Daniel Hughes <DHughes@spartaglobal.com>
2019-07-26 10:39:28 +0100

    update

--- oop_animal_class.py
+++ oop_animal_class.py
@@ -1,14 +1,25 @@
 
 class Animal:
 
-    def __init__(self):
-        pass
+    def __init__(self, name, age, species):
+        self.name = name
+        self.age = age
+        self.species = species
+        self.alive = True
+        self.number_animal_eaten = 0
 
     def sleep(self):
         return "Zzzzzzzz"
 
     def eat(self, food):
-        pass
+        self.number_animal_eaten += 1
+        return "nom nom"
+
+    def shout_own_name(self):
+        return "MY NAME IS " + self.name.upper()
+
+
+
 
 
 
--- run.py
+++ run.py
@@ -1,3 +1,9 @@
 from oop_animal_class import *
 
-print(sleep())
+lion1 = Animal("Dave", 4, "Lion")
+# print(lion1.name)
+# print(lion1.sleep())
+
+print(lion1.eat("food"))
+print(lion1.number_animal_eaten)
+print(lion1.shout_own_name())
